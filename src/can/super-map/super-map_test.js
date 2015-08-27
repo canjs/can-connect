5d398d456bfa078c33823f085df21420dd2da1f7
Justin Meyer <justinbmeyer@gmail.com>
2015-08-27 13:12:18 -0500

    test for #8

--- src/can/super-map/super-map_test.js
+++ src/can/super-map/super-map_test.js
@@ -3,6 +3,8 @@ var fixture = require("can/util/fixture/fixture");
 var Map = require("can/map/map");
 var superMap = require("can-connect/can/super-map/");
 
+QUnit.module("can-connect/can/super-map");
+
 QUnit.test("uses idProp", function(){
 
 	var Restaurant = Map.extend({});
@@ -12,6 +14,30 @@ QUnit.test("uses idProp", function(){
 		idProp: '_id',
 		Map: Restaurant,
 		List: Restaurant.List,
+		name: "restaurant"
+	});
+
+	fixture({
+		"GET /api/restaurants/{_id}": function(request){
+			return {id: 5};
+		}
+	});
+
+	stop();
+	connection.getData({_id: 5}).then(function(data){
+		deepEqual(data, {id: 5}, "findOne");
+		start();
+	});
+
+
+});
+
+
+QUnit.test("creates map if none is provided (#8)", function(){
+
+	var connection = superMap({
+		url: "/api/restaurants",
+		idProp: '_id',
 		name: "restaurant"
 	});
 
