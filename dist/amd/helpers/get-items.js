54e37907434b9a71cab98f523e5f5fdedf941e86
David Luecke <daff@neyeon.com>
2015-10-08 16:42:20 -0700

    0.3.1

---
+++ dist/amd/helpers/get-items.js
@@ -0,0 +1,12 @@
+/*can-connect@0.3.0#helpers/get-items*/
+define(function (require, exports, module) {
+    var isArray = require('./helpers').isArray;
+    module.exports = function (data) {
+        if (isArray(data)) {
+            return data;
+        } else {
+            return data.data;
+        }
+    };
+});
+//# sourceMappingURL=get-items.js.map
