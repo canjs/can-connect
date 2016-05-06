4a3a3f09224e76045a61fafc715e2dc453cebd64
Justin Meyer <justinbmeyer@gmail.com>
2016-05-06 14:59:41 -0500

    0.6.0-pre.3

---
+++ dist/cjs/can/super-map/super-map.js
@@ -0,0 +1,44 @@
+/*can-connect@0.6.0-pre.2#can/super-map/super-map*/
+var connect = require('../../can-connect.js');
+require('../../constructor/constructor.js');
+require('../map/map.js');
+require('../../constructor/store/store.js');
+require('../../constructor/callbacks-once/callbacks-once.js');
+require('../../data/callbacks/callbacks.js');
+require('../../data/callbacks-cache/callbacks-cache.js');
+require('../../data/combine-requests/combine-requests.js');
+require('../../data/inline-cache/inline-cache.js');
+require('../../data/localstorage-cache/localstorage-cache.js');
+require('../../data/parse/parse.js');
+require('../../data/url/url.js');
+require('../../fall-through-cache/fall-through-cache.js');
+require('../../real-time/real-time.js');
+var $ = require('jquery');
+connect.superMap = function (options) {
+    var behaviors = [
+        'constructor',
+        'can-map',
+        'constructor-store',
+        'data-callbacks',
+        'data-combine-requests',
+        'data-inline-cache',
+        'data-parse',
+        'data-url',
+        'real-time',
+        'constructor-callbacks-once'
+    ];
+    if (typeof localStorage !== 'undefined') {
+        if (!options.cacheConnection) {
+            options.cacheConnection = connect(['data-localstorage-cache'], {
+                name: options.name + 'Cache',
+                idProp: options.idProp,
+                algebra: options.algebra
+            });
+        }
+        behaviors.push('data-callbacks-cache', 'fall-through-cache');
+    }
+    options.ajax = $.ajax;
+    return connect(behaviors, options);
+};
+module.exports = connect.superMap;
+//# sourceMappingURL=super-map.js.map
---
+++ dist/cjs/helpers/set-add.js
@@ -0,0 +1,12 @@
+/*can-connect@0.6.0-pre.2#helpers/set-add*/
+var canSet = require('can-set');
+module.exports = function (connection, setItems, items, item, algebra) {
+    var index = canSet.index(setItems, items, item, algebra);
+    if (index === undefined) {
+        index = items.length;
+    }
+    var copy = items.slice(0);
+    copy.splice(index, 0, item);
+    return copy;
+};
+//# sourceMappingURL=set-add.js.map
