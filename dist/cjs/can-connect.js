c61f7865fd7531e3609671d1cb38107e4e763927
Justin Meyer <justinbmeyer@gmail.com>
2016-07-19 15:58:34 -0500

    0.6.0-pre.11

---
+++ dist/cjs/can-connect.js
@@ -0,0 +1,6 @@
+/*can-connect@0.6.0-pre.10#can-connect*/
+var connect = require('./connect.js');
+var base = require('./base/base.js');
+connect.base = base;
+module.exports = connect;
+//# sourceMappingURL=can-connect.js.map
