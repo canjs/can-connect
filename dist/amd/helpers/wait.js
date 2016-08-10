b67969b918c36150372dde222b8e52a5788ff90a
Justin Meyer <justinbmeyer@gmail.com>
2016-08-10 17:02:45 -0500

    0.6.0-pre.13

---
+++ dist/amd/helpers/wait.js
@@ -0,0 +1,36 @@
+/*can-connect@0.6.0-pre.12#helpers/wait*/
+define(function (require, exports, module) {
+    var each = require('can-util/js/each');
+    module.exports = addToCanWaitData;
+    function sortedSetJson(set) {
+        if (set == null) {
+            return set;
+        } else {
+            var sorted = {};
+            var keys = [];
+            for (var k in set) {
+                keys.push(k);
+            }
+            keys.sort();
+            each(keys, function (prop) {
+                sorted[prop] = set[prop];
+            });
+            return JSON.stringify(sorted);
+        }
+    }
+    function addToCanWaitData(promise, name, set) {
+        if (typeof canWait !== 'undefined' && canWait.data) {
+            var addToData = canWait(function (resp) {
+                var data = {};
+                var keyData = data[name] = {};
+                keyData[sortedSetJson(set)] = typeof resp.serialize === 'function' ? resp.serialize() : resp;
+                canWait.data({ pageData: data });
+                return resp;
+            });
+            promise.then(null, addToData);
+            return promise.then(addToData);
+        }
+        return promise;
+    }
+});
+//# sourceMappingURL=wait.js.map
