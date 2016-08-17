a7d61528ee7aa80c7fc96f078ed774b23ee6ce6c
Justin Meyer <justinbmeyer@gmail.com>
2016-08-17 17:14:26 -0500

    0.6.0-pre.15

---
+++ dist/amd/can-connect.js
@@ -0,0 +1,8 @@
+/*can-connect@0.6.0-pre.14#can-connect*/
+define(function (require, exports, module) {
+    var connect = require('./connect');
+    var base = require('./base/base');
+    connect.base = base;
+    module.exports = connect;
+});
+//# sourceMappingURL=can-connect.js.map
