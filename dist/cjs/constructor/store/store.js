44486a1d50bc609f25b6819b29375be81f5bd67a
Justin Meyer <justinbmeyer@gmail.com>
2016-09-17 19:06:32 -0500

    0.6.0-pre.20

---
+++ dist/cjs/constructor/store/store.js
@@ -0,0 +1,180 @@
+/*can-connect@0.6.0-pre.19#constructor/store/store*/
+var connect = require('../../can-connect.js');
+var WeakReferenceMap = require('../../helpers/weak-reference-map.js');
+var sortedSetJSON = require('../../helpers/sorted-set-json.js');
+var canEvent = require('can-event');
+var assign = require('can-util/js/assign/assign');
+var pendingRequests = 0;
+var noRequestsTimer = null;
+var requests = {
+    increment: function (connection) {
+        pendingRequests++;
+        clearTimeout(noRequestsTimer);
+    },
+    decrement: function (connection) {
+        pendingRequests--;
+        if (pendingRequests === 0) {
+            noRequestsTimer = setTimeout(function () {
+                requests.dispatch('end');
+            }, 10);
+        }
+    },
+    count: function () {
+        return pendingRequests;
+    }
+};
+assign(requests, canEvent);
+var constructorStore = connect.behavior('constructor/store', function (baseConnect) {
+    var behavior = {
+        instanceStore: new WeakReferenceMap(),
+        listStore: new WeakReferenceMap(),
+        _requestInstances: {},
+        _requestLists: {},
+        _finishedRequest: function () {
+            var id;
+            requests.decrement(this);
+            if (requests.count() === 0) {
+                for (id in this._requestInstances) {
+                    this.instanceStore.deleteReference(id);
+                }
+                this._requestInstances = {};
+                for (id in this._requestLists) {
+                    this.listStore.deleteReference(id);
+                }
+                this._requestLists = {};
+            }
+        },
+        addInstanceReference: function (instance, id) {
+            this.instanceStore.addReference(id || this.id(instance), instance);
+        },
+        addInstanceMetaData: function (instance, name, value) {
+            var data = this.instanceStore.set[this.id(instance)];
+            if (data) {
+                data[name] = value;
+            }
+        },
+        getInstanceMetaData: function (instance, name) {
+            var data = this.instanceStore.set[this.id(instance)];
+            if (data) {
+                return data[name];
+            }
+        },
+        deleteInstanceMetaData: function (instance, name) {
+            var data = this.instanceStore.set[this.id(instance)];
+            delete data[name];
+        },
+        deleteInstanceReference: function (instance) {
+            this.instanceStore.deleteReference(this.id(instance), instance);
+        },
+        addListReference: function (list, set) {
+            var id = sortedSetJSON(set || this.listSet(list));
+            if (id) {
+                this.listStore.addReference(id, list);
+            }
+        },
+        deleteListReference: function (list, set) {
+            var id = sortedSetJSON(set || this.listSet(list));
+            if (id) {
+                this.listStore.deleteReference(id, list);
+            }
+        },
+        hydratedInstance: function (instance) {
+            if (requests.count() > 0) {
+                var id = this.id(instance);
+                if (!this._requestInstances[id]) {
+                    this.addInstanceReference(instance);
+                    this._requestInstances[id] = instance;
+                }
+            }
+        },
+        hydrateInstance: function (props) {
+            var id = this.id(props);
+            if ((id || id === 0) && this.instanceStore.has(id)) {
+                var storeInstance = this.instanceStore.get(id);
+                this.updatedInstance(storeInstance, props);
+                return storeInstance;
+            }
+            var instance = baseConnect.hydrateInstance.call(this, props);
+            this.hydratedInstance(instance);
+            return instance;
+        },
+        hydratedList: function (list, set) {
+            if (requests.count() > 0) {
+                var id = sortedSetJSON(set || this.listSet(list));
+                if (id) {
+                    if (!this._requestLists[id]) {
+                        this.addListReference(list, set);
+                        this._requestLists[id] = list;
+                    }
+                }
+            }
+        },
+        hydrateList: function (listData, set) {
+            set = set || this.listSet(listData);
+            var id = sortedSetJSON(set);
+            if (id && this.listStore.has(id)) {
+                var storeList = this.listStore.get(id);
+                this.updatedList(storeList, listData, set);
+                return storeList;
+            }
+            var list = baseConnect.hydrateList.call(this, listData, set);
+            this.hydratedList(list, set);
+            return list;
+        },
+        getList: function (params) {
+            var self = this;
+            requests.increment(this);
+            var promise = baseConnect.getList.call(this, params);
+            promise.then(function (instances) {
+                self._finishedRequest();
+            }, function () {
+                self._finishedRequest();
+            });
+            return promise;
+        },
+        get: function (params) {
+            var self = this;
+            requests.increment(this);
+            var promise = baseConnect.get.call(this, params);
+            promise.then(function (instance) {
+                self._finishedRequest();
+            }, function () {
+                self._finishedRequest();
+            });
+            return promise;
+        },
+        save: function (instance) {
+            var self = this;
+            requests.increment(this);
+            var updating = !this.isNew(instance);
+            if (updating) {
+                this.addInstanceReference(instance);
+            }
+            var promise = baseConnect.save.call(this, instance);
+            promise.then(function (instances) {
+                if (updating) {
+                    self.deleteInstanceReference(instance);
+                }
+                self._finishedRequest();
+            }, function () {
+                self._finishedRequest();
+            });
+            return promise;
+        },
+        destroy: function (instance) {
+            var self = this;
+            requests.increment(this);
+            var promise = baseConnect.destroy.call(this, instance);
+            promise.then(function (instance) {
+                self._finishedRequest();
+            }, function () {
+                self._finishedRequest();
+            });
+            return promise;
+        }
+    };
+    return behavior;
+});
+constructorStore.requests = requests;
+module.exports = constructorStore;
+//# sourceMappingURL=store.js.map
