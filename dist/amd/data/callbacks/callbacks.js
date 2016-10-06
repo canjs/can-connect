2996acf24f56497fa843e95ee10524da26919489
Garrett Reed <garrett@garrettreed.co>
2016-10-06 11:49:47 -0400

    0.6.0-pre.22

---
+++ dist/amd/data/callbacks/callbacks.js
@@ -0,0 +1,28 @@
+/*can-connect@0.6.0-pre.21#data/callbacks/callbacks*/
+define(function (require, exports, module) {
+    var connect = require('../../can-connect');
+    var each = require('can-util/js/each');
+    var pairs = {
+        getListData: 'gotListData',
+        createData: 'createdData',
+        updateData: 'updatedData',
+        destroyData: 'destroyedData'
+    };
+    module.exports = connect.behavior('data/callbacks', function (baseConnect) {
+        var behavior = {};
+        each(pairs, function (callbackName, name) {
+            behavior[name] = function (params, cid) {
+                var self = this;
+                return baseConnect[name].call(this, params).then(function (data) {
+                    if (self[callbackName]) {
+                        return self[callbackName].call(self, data, params, cid);
+                    } else {
+                        return data;
+                    }
+                });
+            };
+        });
+        return behavior;
+    });
+});
+//# sourceMappingURL=callbacks.js.map
---
+++ dist/amd/helpers/overwrite.js
@@ -0,0 +1,15 @@
+/*can-connect@0.6.0-pre.21#helpers/overwrite*/
+define(function (require, exports, module) {
+    module.exports = function (d, s, id) {
+        for (var prop in d) {
+            if (prop !== id && !(prop in s)) {
+                delete d[prop];
+            }
+        }
+        for (prop in s) {
+            d[prop] = s[prop];
+        }
+        return d;
+    };
+});
+//# sourceMappingURL=overwrite.js.map
