b2448d1af3109ddcb9a2f1fae68be3a41daf1603
Kevin Phillips <kphillips86@gmail.com>
2016-10-11 17:15:22 -0500

    0.6.0-pre.23

---
+++ dist/amd/can-connect.js
@@ -0,0 +1,9 @@
+/*can-connect@0.6.0-pre.22#can-connect*/
+define(function (require, exports, module) {
+    var connect = require('./connect');
+    var base = require('./base/base');
+    var ns = require('can-util/namespace');
+    connect.base = base;
+    module.exports = ns.connect = connect;
+});
+//# sourceMappingURL=can-connect.js.map
---
+++ dist/amd/helpers/set-add.js
@@ -0,0 +1,14 @@
+/*can-connect@0.6.0-pre.22#helpers/set-add*/
+define(function (require, exports, module) {
+    var canSet = require('can-set');
+    module.exports = function (connection, setItems, items, item, algebra) {
+        var index = canSet.index(setItems, items, item, algebra);
+        if (index === undefined) {
+            index = items.length;
+        }
+        var copy = items.slice(0);
+        copy.splice(index, 0, item);
+        return copy;
+    };
+});
+//# sourceMappingURL=set-add.js.map
