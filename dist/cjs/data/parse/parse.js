486f60c8ae801ac0a2612cfabb33a0ce1b229288
David Luecke <daff@neyeon.com>
2016-10-17 12:23:29 -0700

    1.0.0

---
+++ dist/cjs/data/parse/parse.js
@@ -0,0 +1,57 @@
+/*can-connect@0.6.1#data/parse/parse*/
+var connect = require('../../can-connect.js');
+var each = require('can-util/js/each/each');
+var isArray = require('can-util/js/is-array/is-array');
+var string = require('can-util/js/string/string');
+module.exports = connect.behavior('data/parse', function (baseConnect) {
+    var behavior = {
+        parseListData: function (responseData) {
+            if (baseConnect.parseListData) {
+                responseData = baseConnect.parseListData.apply(this, arguments);
+            }
+            var result;
+            if (isArray(responseData)) {
+                result = { data: responseData };
+            } else {
+                var prop = this.parseListProp || 'data';
+                responseData.data = string.getObject(prop, responseData);
+                result = responseData;
+                if (prop !== 'data') {
+                    delete responseData[prop];
+                }
+                if (!isArray(result.data)) {
+                    throw new Error('Could not get any raw data while converting using .parseListData');
+                }
+            }
+            var arr = [];
+            for (var i = 0; i < result.data.length; i++) {
+                arr.push(this.parseInstanceData(result.data[i]));
+            }
+            result.data = arr;
+            return result;
+        },
+        parseInstanceData: function (props) {
+            if (baseConnect.parseInstanceData) {
+                props = baseConnect.parseInstanceData.apply(this, arguments) || props;
+            }
+            return this.parseInstanceProp ? string.getObject(this.parseInstanceProp, props) || props : props;
+        }
+    };
+    each(pairs, function (parseFunction, name) {
+        behavior[name] = function (params) {
+            var self = this;
+            return baseConnect[name].call(this, params).then(function () {
+                return self[parseFunction].apply(self, arguments);
+            });
+        };
+    });
+    return behavior;
+});
+var pairs = {
+    getListData: 'parseListData',
+    getData: 'parseInstanceData',
+    createData: 'parseInstanceData',
+    updateData: 'parseInstanceData',
+    destroyData: 'parseInstanceData'
+};
+//# sourceMappingURL=parse.js.map
---
+++ dist/cjs/helpers/get-id-props.js
@@ -0,0 +1,17 @@
+/*can-connect@0.6.1#helpers/get-id-props*/
+module.exports = function (connection) {
+    var ids = [], algebra = this.algebra;
+    if (algebra && algebra.clauses && algebra.clauses.id) {
+        for (var prop in algebra.clauses.id) {
+            ids.push(prop);
+        }
+    }
+    if (connection.idProp && !ids.length) {
+        ids.push(connection.idProp);
+    }
+    if (!ids.length) {
+        ids.push('id');
+    }
+    return ids;
+};
+//# sourceMappingURL=get-id-props.js.map
