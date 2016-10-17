198a46ceae6ebe6613cf76f2344c18de2697aeb6
David Luecke <daff@neyeon.com>
2016-10-17 13:31:01 -0700

    1.0.1

---
+++ dist/amd/base/base.js
@@ -0,0 +1,31 @@
+/*can-connect@1.0.0#base/base*/
+define(function (require, exports, module) {
+    var connect = require('../connect');
+    module.exports = connect.behavior('base', function (base) {
+        return {
+            id: function (instance) {
+                var ids = [], algebra = this.algebra;
+                if (algebra && algebra.clauses && algebra.clauses.id) {
+                    for (var prop in algebra.clauses.id) {
+                        ids.push(instance[prop]);
+                    }
+                }
+                if (this.idProp && !ids.length) {
+                    ids.push(instance[this.idProp]);
+                }
+                if (!ids.length) {
+                    ids.push(instance.id);
+                }
+                return ids.length > 1 ? ids.join('@|@') : ids[0];
+            },
+            idProp: base.idProp || 'id',
+            listSet: function (list) {
+                return list[this.listSetProp];
+            },
+            listSetProp: '__listSet',
+            init: function () {
+            }
+        };
+    });
+});
+//# sourceMappingURL=base.js.map
