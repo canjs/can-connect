46d65da004ded3c01e7574d04d0cf3f89cf2f5e6
Justin Meyer <justinbmeyer@gmail.com>
2016-10-17 09:37:51 -0500

    0.6.0

---
+++ dist/cjs/data/callbacks-cache/callbacks-cache.js
@@ -0,0 +1,20 @@
+/*can-connect@0.6.0-pre.26#data/callbacks-cache/callbacks-cache*/
+var connect = require('../../can-connect.js');
+var assign = require('can-util/js/assign/assign');
+var each = require('can-util/js/each/each');
+var pairs = {
+    createdData: 'createData',
+    updatedData: 'updateData',
+    destroyedData: 'destroyData'
+};
+module.exports = connect.behavior('data/callbacks-cache', function (baseConnect) {
+    var behavior = {};
+    each(pairs, function (cacheCallback, dataCallbackName) {
+        behavior[dataCallbackName] = function (data, set, cid) {
+            this.cacheConnection[cacheCallback](assign(assign({}, set), data));
+            return baseConnect[dataCallbackName].call(this, data, set, cid);
+        };
+    });
+    return behavior;
+});
+//# sourceMappingURL=callbacks-cache.js.map
