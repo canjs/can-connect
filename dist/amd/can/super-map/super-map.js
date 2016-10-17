7a832ce9d2169744217f319f48f9a2767e027b36
Justin Meyer <justinbmeyer@gmail.com>
2016-10-17 12:38:39 -0500

    0.6.1

---
+++ dist/amd/can/super-map/super-map.js
@@ -0,0 +1,48 @@
+/*can-connect@0.6.0#can/super-map/super-map*/
+define(function (require, exports, module) {
+    var connect = require('../../can-connect');
+    var constructor = require('../../constructor/constructor');
+    var canMap = require('../map/map');
+    var canRef = require('../ref/ref');
+    var constructorStore = require('../../constructor/store/store');
+    var dataCallbacks = require('../../data/callbacks/callbacks');
+    var callbacksCache = require('../../data/callbacks-cache/callbacks-cache');
+    var combineRequests = require('../../data/combine-requests/combine-requests');
+    var localCache = require('../../data/localstorage-cache/localstorage-cache');
+    var dataParse = require('../../data/parse/parse');
+    var dataUrl = require('../../data/url/url');
+    var fallThroughCache = require('../../fall-through-cache/fall-through-cache');
+    var realTime = require('../../real-time/real-time');
+    var callbacksOnce = require('../../constructor/callbacks-once/callbacks-once');
+    var $ = require('jquery');
+    connect.superMap = function (options) {
+        var behaviors = [
+            constructor,
+            canMap,
+            canRef,
+            constructorStore,
+            dataCallbacks,
+            combineRequests,
+            dataParse,
+            dataUrl,
+            realTime,
+            callbacksOnce
+        ];
+        if (typeof localStorage !== 'undefined') {
+            if (!options.cacheConnection) {
+                options.cacheConnection = connect([localCache], {
+                    name: options.name + 'Cache',
+                    idProp: options.idProp,
+                    algebra: options.algebra
+                });
+            }
+            behaviors.push(callbacksCache, fallThroughCache);
+        }
+        if ($ && $.ajax) {
+            options.ajax = $.ajax;
+        }
+        return connect(behaviors, options);
+    };
+    module.exports = connect.superMap;
+});
+//# sourceMappingURL=super-map.js.map
---
+++ dist/cjs/helpers/overwrite.js
@@ -0,0 +1,13 @@
+/*can-connect@0.6.0#helpers/overwrite*/
+module.exports = function (d, s, id) {
+    for (var prop in d) {
+        if (prop !== id && !(prop in s)) {
+            delete d[prop];
+        }
+    }
+    for (prop in s) {
+        d[prop] = s[prop];
+    }
+    return d;
+};
+//# sourceMappingURL=overwrite.js.map
