9c69ddcb3e74660183b533fb11bc98f71caad7bc
Kevin Phillips <kphillips86@gmail.com>
2016-11-10 11:04:37 -0600

    1.0.10

---
+++ dist/amd/helpers/get-id-props.js
@@ -0,0 +1,19 @@
+/*can-connect@1.0.9#helpers/get-id-props*/
+define(function (require, exports, module) {
+    module.exports = function (connection) {
+        var ids = [], algebra = this.algebra;
+        if (algebra && algebra.clauses && algebra.clauses.id) {
+            for (var prop in algebra.clauses.id) {
+                ids.push(prop);
+            }
+        }
+        if (connection.idProp && !ids.length) {
+            ids.push(connection.idProp);
+        }
+        if (!ids.length) {
+            ids.push('id');
+        }
+        return ids;
+    };
+});
+//# sourceMappingURL=get-id-props.js.map
---
+++ dist/cjs/can-connect.js
@@ -0,0 +1,7 @@
+/*can-connect@1.0.9#can-connect*/
+var connect = require('./connect.js');
+var base = require('./base/base.js');
+var ns = require('can-util/namespace');
+connect.base = base;
+module.exports = ns.connect = connect;
+//# sourceMappingURL=can-connect.js.map
