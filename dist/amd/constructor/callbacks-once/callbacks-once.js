eec31156776c3e0556b64c0248ab4b03bf5ad4b9
Kevin Phillips <kphillips86@gmail.com>
2016-11-17 10:09:00 -0600

    1.0.12

---
+++ dist/amd/constructor/callbacks-once/callbacks-once.js
@@ -0,0 +1,27 @@
+/*can-connect@1.0.11#constructor/callbacks-once/callbacks-once*/
+define(function (require, exports, module) {
+    var connect = require('../../can-connect');
+    var sortedSetJSON = require('../../helpers/sorted-set-json');
+    var forEach = [].forEach;
+    var callbacks = [
+        'createdInstance',
+        'updatedInstance',
+        'destroyedInstance'
+    ];
+    module.exports = connect.behavior('constructor/callbacks-once', function (baseConnection) {
+        var behavior = {};
+        forEach.call(callbacks, function (name) {
+            behavior[name] = function (instance, data) {
+                var lastSerialized = this.getInstanceMetaData(instance, 'last-data');
+                var serialize = sortedSetJSON(data), serialized = sortedSetJSON(this.serializeInstance(instance));
+                if (lastSerialized !== serialize && serialized !== serialize) {
+                    var result = baseConnection[name].apply(this, arguments);
+                    this.addInstanceMetaData(instance, 'last-data', serialize);
+                    return result;
+                }
+            };
+        });
+        return behavior;
+    });
+});
+//# sourceMappingURL=callbacks-once.js.map
