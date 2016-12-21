3f09cbb1a5d2d2a19c58ef11b63f99f67e5e65bf
Marshall Thompson <marshall@creativeideal.net>
2016-12-20 17:07:25 -0700

    1.0.21

---
+++ dist/amd/can-connect.js
@@ -0,0 +1,9 @@
+/*can-connect@1.0.20#can-connect*/
+define(function (require, exports, module) {
+    var connect = require('./connect');
+    var base = require('./base/base');
+    var ns = require('can-namespace');
+    connect.base = base;
+    module.exports = ns.connect = connect;
+});
+//# sourceMappingURL=can-connect.js.map
