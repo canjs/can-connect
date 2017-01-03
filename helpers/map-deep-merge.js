ab8619849b15d81b721fd19804367b4a8a24d4fe
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-01-03 12:41:06 -0500

    added docs to smartMerge

--- helpers/map-deep-merge.js
+++ helpers/map-deep-merge.js
@@ -12,10 +12,108 @@
 var DefineList = require('can-define/list/list');
 var diff = require('can-util/js/diff/diff');
 
-/*
- * Main method exported by the module.
- * @param {can-define/map/map | can-define/list/list} instance Instance to apply smartMerge to.
- * @param {Object} props Data to be merged to the provided instance.
+/**
+ * @module {function} can-connect/helpers/map-deep-merge mapDeepMerge
+ * @parent can-connect.modules
+ *
+ * Perform a smart merge of deeply nested maps and lists.
+ *
+ * @signature `mapDeepMerge( instance, data )`
+ *
+ *   Merges changes into the `can-define/map/map` or `can-define/list/list` instance considering the nested structure.
+ *   Dispatches update events for the exact changes that needs to be applied.
+ *
+ *   @param {can-define/map/map | can-define/map/list} instance An instance to apply a merge to.
+ *   @param {Object | Array} data An object or array with the updated data.
+ *
+ * @body
+ *
+ * ## Use
+ *
+ * This method is especially helpful when is used with `can-connect` models that contain nested data and when
+ * the data is live-bound to a template. It makes the update only fire the required minimum changes.
+ *
+ * ## With can-define/map/map
+ *
+ * Lets say we have a `ContributionMonth` map that contains a list of OS projects and an author:
+ *
+ * ```js
+ *  var DefineMap = require("can-define/map/map");
+ *
+ *  var Author = DefineMap.extend({});
+ *  Author.algebra = new set.Algebra( set.props.id("id") );
+ *
+ *  var OSProject = DefineMap.extend({});
+ *  OSProject.algebra = new set.Algebra( set.props.id("id") );
+ *
+ *  var ContributionMonth = DefineMap.extend({
+ *    author: Author,
+ *    osProjects: OSProject.List
+ *  });
+ *
+ * 	var myMonth = new ContributionMonth({
+ * 	    id: 1,
+ * 	    month: "Feb",
+ * 	    osProjects: [ { id: 1, title: "canjs" }, {id: 2, title: "jQuery++"} ],
+ * 	    author: {id: 5, name: "ilya"}
+ * 	});
+ * ```
+ *
+ * And we receive the following updated data:
+ *
+ * ```js
+ *  var updatedData = {
+ *      id: 1,
+ *      month: "February",
+ *      osProjects: [ { id: 1, title: "CanJS" }, {id: 3, title: "StealJS"}, {id: 2, title: "jQuery++"} ],
+ *      author: {id: 6, name: "ilya"}
+ *  };
+ * ```
+ *
+ * To make our template engine apply only the minimun changes we need the map to dispatch only the following updates:
+ *
+ * ```js
+ *  contributionMonth.name = "February";                    		// 1 - a property update
+ *  contributionMonth.osProjects[0].name = "CanJS";         		// 2 - a property update on an item of a list
+ *  contributionMonth.osProjects.splice(1,0, new OSProject({id: 3, name: "StealJS"}) )     // 3 - item insertion
+ *  contributionMonth.author = hydrateInstance( {id: 6, name: "ilya"} )                    // 4 - a map replacement (`id` is different)
+ * ```
+ *
+ * To have this setup we now can update `myMonth` with `updatedData` using our `smartMerge`:
+ *
+ * ```js
+ * var smartMerge = require("can-connect/helpers/map-deep-merge");
+ *
+ * smartMerge( myMonth, updatedData );
+ * ```
+ *
+ * ### With can-connect
+ *
+ * We can use our `ContributionMonth` with `can-connect` by creating a new behavior which will perform item update using
+ * `can-connect/helpers/map-deep-merge` helper:
+ *
+ * ```js
+ * var connect = require("can-connect");
+ * var smartMerge = require("can-connect/helpers/map-deep-merge");
+ *
+ * var canMapMergeBehaviour = {
+ *     updatedInstance: function(instance, props){
+ *         smartMerge( instance, props );
+ *         canMap.callbackInstanceEvents("updated", instance);
+ *     }
+ * };
+ *
+ * connect([dataUrl, constructor, constructorStore, canMap, canMapMergeBehaviour], {
+ *     Map: ContributionMonth,
+ *     url: "localhost:8080/contribution-month"
+ * });
+ *
+ * var item = new ContributionMonth(origData);
+ *
+ * item.save();
+ * ```
+ *
+ * Now when we receive the `updatedData` on the save response from server we will get only the minimum changes dispatched.
  */
 function smartMerge( instance, props ) {
 	if( instance instanceof DefineList ) {
