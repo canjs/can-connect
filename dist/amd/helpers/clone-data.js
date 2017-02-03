b816f405b2c785d035891ba5cdc3202fe41cfb5f
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-03 13:43:08 -0500

    1.3.1

---
+++ dist/amd/helpers/clone-data.js
@@ -0,0 +1,9 @@
+/*can-connect@1.3.0#helpers/clone-data*/
+define(function (require, exports, module) {
+    var isArray = require('can-util/js/is-array');
+    var deepAssign = require('can-util/js/deep-assign');
+    module.exports = function (data) {
+        return isArray(data) ? data.slice(0) : deepAssign({}, data);
+    };
+});
+//# sourceMappingURL=clone-data.js.map
