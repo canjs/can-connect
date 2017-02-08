4f2fb202902fb8b9a57dd3fafb30cf7f00923a67
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-08 18:27:41 -0500

    1.3.3

---
+++ dist/cjs/helpers/get-id-props.js
@@ -0,0 +1,17 @@
+/*can-connect@1.3.2#helpers/get-id-props*/
+module.exports = function (connection) {
+    var ids = [], algebra = this.algebra;
+    if (algebra && algebra.clauses && algebra.clauses.id) {
+        for (var prop in algebra.clauses.id) {
+            ids.push(prop);
+        }
+    }
+    if (connection.idProp && !ids.length) {
+        ids.push(connection.idProp);
+    }
+    if (!ids.length) {
+        ids.push('id');
+    }
+    return ids;
+};
+//# sourceMappingURL=get-id-props.js.map
