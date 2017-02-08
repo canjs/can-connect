56dccb25bb9148195a61443f8cc6708c1ac1109b
Kyle Gifford <kyle@kylegifford.com>
2017-02-08 15:08:27 -0600

    1.3.2

---
+++ dist/amd/helpers/get-items.js
@@ -0,0 +1,12 @@
+/*can-connect@1.3.1#helpers/get-items*/
+define(function (require, exports, module) {
+    var isArray = require('can-util/js/is-array');
+    module.exports = function (data) {
+        if (isArray(data)) {
+            return data;
+        } else {
+            return data.data;
+        }
+    };
+});
+//# sourceMappingURL=get-items.js.map
---
+++ dist/cjs/helpers/clone-data.js
@@ -0,0 +1,7 @@
+/*can-connect@1.3.1#helpers/clone-data*/
+var isArray = require('can-util/js/is-array/is-array');
+var deepAssign = require('can-util/js/deep-assign/deep-assign');
+module.exports = function (data) {
+    return isArray(data) ? data.slice(0) : deepAssign({}, data);
+};
+//# sourceMappingURL=clone-data.js.map
