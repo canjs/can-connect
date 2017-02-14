962f88178391f01906fa119e64c0e3350839bde1
Kevin Phillips <kphillips86@gmail.com>
2017-02-13 21:17:18 -0600

    using idProp from set algebra in supermap

--- can/super-map/super-map_test.js
+++ can/super-map/super-map_test.js
@@ -2,6 +2,7 @@ var QUnit = require("steal-qunit");
 var fixture = require("can-fixture");
 var Map = require("can-map");
 var superMap = require("can-connect/can/super-map/");
+var set = require("can-set");
 
 QUnit.module("can-connect/can/super-map");
 
@@ -81,4 +82,33 @@ QUnit.test("allow other caches (#59)", function(){
 	connection.getData({_id: 5}).then(function(data){
 		start();
 	});
+});
+
+QUnit.test("uses idProp from algebra (#255)", function(){
+
+	var Restaurant = Map.extend({});
+
+	var connection = superMap({
+		url: "/api/restaurants",
+		Map: Restaurant,
+		List: Restaurant.List,
+		name: "restaurant",
+		algebra: new set.Algebra(
+		   set.comparators.id("_id")
+		)
+	});
+
+	fixture({
+		"GET /api/restaurants/{_id}": function(request){
+			return {id: 5};
+		}
+	});
+
+	stop();
+	connection.getData({_id: 5}).then(function(data){
+		deepEqual(data, {id: 5}, "findOne");
+		start();
+	});
+
+
 });
