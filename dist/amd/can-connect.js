9aa2727f2c1cb663e92017bf608bfbd265c5e3bb
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-17 15:50:50 -0500

    1.3.6

---
+++ dist/amd/can-connect.js
@@ -0,0 +1,9 @@
+/*can-connect@1.3.5#can-connect*/
+define(function (require, exports, module) {
+    var connect = require('./connect');
+    var base = require('./base/base');
+    var ns = require('can-namespace');
+    connect.base = base;
+    module.exports = ns.connect = connect;
+});
+//# sourceMappingURL=can-connect.js.map
