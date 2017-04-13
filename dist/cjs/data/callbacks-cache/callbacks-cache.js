c51ad0a2e887f83b3f7487a5126c49e99e8b4d0c
Kevin Phillips <kevin@bitovi.com>
2017-04-13 10:46:45 -0500

    1.3.10

---
+++ dist/cjs/data/callbacks-cache/callbacks-cache.js
@@ -0,0 +1,20 @@
+/*can-connect@1.3.9#data/callbacks-cache/callbacks-cache*/
+var connect = require('../../can-connect.js');
+var assign = require('can-util/js/assign/assign');
+var each = require('can-util/js/each/each');
+var pairs = {
+    createdData: 'createData',
+    updatedData: 'updateData',
+    destroyedData: 'destroyData'
+};
+module.exports = connect.behavior('data/callbacks-cache', function (baseConnection) {
+    var behavior = {};
+    each(pairs, function (cacheCallback, dataCallbackName) {
+        behavior[dataCallbackName] = function (data, set, cid) {
+            this.cacheConnection[cacheCallback](assign(assign({}, set), data));
+            return baseConnection[dataCallbackName].call(this, data, set, cid);
+        };
+    });
+    return behavior;
+});
+//# sourceMappingURL=callbacks-cache.js.map
---
+++ dist/cjs/helpers/get-id-props.js
@@ -0,0 +1,17 @@
+/*can-connect@1.3.9#helpers/get-id-props*/
+module.exports = function (connection) {
+    var ids = [], algebra = connection.algebra;
+    if (algebra && algebra.clauses && algebra.clauses.id) {
+        for (var prop in algebra.clauses.id) {
+            ids.push(prop);
+        }
+    }
+    if (connection.idProp && !ids.length) {
+        ids.push(connection.idProp);
+    }
+    if (!ids.length) {
+        ids.push('id');
+    }
+    return ids;
+};
+//# sourceMappingURL=get-id-props.js.map
