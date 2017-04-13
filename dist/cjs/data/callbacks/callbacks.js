2443f77577e1e96e114e33f25c2ea55cfba5deb2
Bradley Momberger <brad@bitovi.com>
2017-04-13 11:19:58 -0400

    1.3.9

---
+++ dist/cjs/data/callbacks/callbacks.js
@@ -0,0 +1,26 @@
+/*can-connect@1.3.8#data/callbacks/callbacks*/
+var connect = require('../../can-connect.js');
+var each = require('can-util/js/each/each');
+var pairs = {
+    getListData: 'gotListData',
+    createData: 'createdData',
+    updateData: 'updatedData',
+    destroyData: 'destroyedData'
+};
+module.exports = connect.behavior('data/callbacks', function (baseConnection) {
+    var behavior = {};
+    each(pairs, function (callbackName, name) {
+        behavior[name] = function (params, cid) {
+            var self = this;
+            return baseConnection[name].call(this, params).then(function (data) {
+                if (self[callbackName]) {
+                    return self[callbackName].call(self, data, params, cid);
+                } else {
+                    return data;
+                }
+            });
+        };
+    });
+    return behavior;
+});
+//# sourceMappingURL=callbacks.js.map
