98dd0964f0f70cfaa43fa7c20cb11a30ca0f6e16
Kevin Phillips <kevin@bitovi.com>
2017-06-16 09:46:32 -0500

    1.5.0-pre.5

---
+++ dist/amd/helpers/get-items.js
@@ -0,0 +1,12 @@
+/*can-connect@1.5.0-pre.4#helpers/get-items*/
+define(function (require, exports, module) {
+    var isArray = require('can-util/js/is-array');
+    module.exports = function (data) {
+        if (isArray(data)) {
+            return data;
+        } else {
+            return data.data;
+        }
+    };
+});
+//# sourceMappingURL=get-items.js.map
