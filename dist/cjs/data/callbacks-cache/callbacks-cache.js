14b072b39cd2a90b4ed89c61714cdb41555902b4
Kevin Phillips <kevin@bitovi.com>
2017-06-23 13:48:48 -0500

    1.5.1

---
+++ dist/cjs/data/callbacks-cache/callbacks-cache.js
@@ -0,0 +1,21 @@
+/*can-connect@1.5.0#data/callbacks-cache/callbacks-cache*/
+var connect = require('../../can-connect.js');
+var assign = require('can-util/js/assign/assign');
+var each = require('can-util/js/each/each');
+var pairs = {
+    createdData: 'createData',
+    updatedData: 'updateData',
+    destroyedData: 'destroyData'
+};
+var callbackCache = connect.behavior('data/callbacks-cache', function (baseConnection) {
+    var behavior = {};
+    each(pairs, function (cacheCallback, dataCallbackName) {
+        behavior[dataCallbackName] = function (data, set, cid) {
+            this.cacheConnection[cacheCallback](assign(assign({}, set), data));
+            return baseConnection[dataCallbackName].call(this, data, set, cid);
+        };
+    });
+    return behavior;
+});
+module.exports = callbackCache;
+//# sourceMappingURL=callbacks-cache.js.map
