6b1ca5433902b2c0702a9f07a053bf82773dc516
Justin Meyer <justinbmeyer@gmail.com>
2017-11-21 14:46:25 -0600

    test with observe.Array and observe.Object

--- can/map/observe-test.js
+++ can/map/observe-test.js
@@ -4,5 +4,5 @@ var QUnit = require("steal-qunit");
 QUnit.module("can-connect/can/map/map with can-observe",{});
 
 require("./test-real-time-super-model")(function(){
-	return {Todo: observe.makeMapType("Todo",{},{}), TodoList: observe.makeListType("TodoList",{},{})};
+	return {Todo: observe.Object.extend("Todo",{},{}), TodoList: observe.Array.extend("TodoList",{},{})};
 });
