b1c480ea97341cfc0ac9953b7acac081e1502447
Bradley Momberger <brad@bitovi.com>
2017-12-18 18:29:14 -0500

    1.5.13

---
+++ dist/cjs/helpers/clone-data.js
@@ -0,0 +1,6 @@
+/*can-connect@1.5.12#helpers/clone-data*/
+var deepAssign = require('can-util/js/deep-assign/deep-assign');
+module.exports = function (data) {
+    return Array.isArray(data) ? data.slice(0) : deepAssign({}, data);
+};
+//# sourceMappingURL=clone-data.js.map
