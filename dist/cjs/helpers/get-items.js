296c4b966987970e4e1b7a94bca11714a79370cd
Chris Andrejewski <christopher.andrejewski@gmail.com>
2018-01-12 09:20:44 -0500

    1.5.17

---
+++ dist/cjs/helpers/get-items.js
@@ -0,0 +1,9 @@
+/*can-connect@1.5.16#helpers/get-items*/
+module.exports = function (data) {
+    if (Array.isArray(data)) {
+        return data;
+    } else {
+        return data.data;
+    }
+};
+//# sourceMappingURL=get-items.js.map
