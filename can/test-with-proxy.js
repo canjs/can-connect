e18550be65d7e0547cd8383cd9edc5a980eb5155
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2018-11-02 17:29:21 +0100

    renaming test modules to follow the standard for proxy

---
+++ can/test-with-proxy.js
@@ -0,0 +1,8 @@
+
+require("./map/map_test");
+require("./map/define-map-test");
+require("./map/observe-test");
+require("./ref/ref-test");
+require("./super-map/super-map_test");
+require("./base-map/base-map_test");
+require("./constructor-hydrate/constructor-hydrate-test");
