73dcffceedf1f4492b3fcf1dc3eac7cad702d900
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2018-11-08 21:01:01 +0100

    organise tests file to fit the standard

--- can/test-with-proxy.js
+++
@@ -1,8 +0,0 @@
-
-require("./map/map_test");
-require("./map/define-map-test");
-require("./map/observe-test");
-require("./ref/ref-test");
-require("./super-map/super-map_test");
-require("./base-map/base-map_test");
-require("./constructor-hydrate/constructor-hydrate-test");
