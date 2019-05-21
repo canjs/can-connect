cf771efb6998424fe718dc66169548c6e3a551f7
Nils Lundquist <nlundqu@gmail.com>
2019-05-21 11:53:00 -0600

    include the "can" behaviors as part of the built module default export

--- all.js
+++ all.js
@@ -16,6 +16,10 @@ connect.dataUrl = require("./data/url/url");
 connect.fallThroughCache = require("./fall-through-cache/fall-through-cache");
 connect.realTime = require("./real-time/real-time");
 
+connect.canMap = require("./can/map/map");
+connect.canRef = require("./can/ref/ref");
+connect.canMerge = require("./can/merge/merge");
+
 connect.superMap = require("./can/super-map/super-map");
 connect.baseMap = require('./can/base-map/base-map');
 
